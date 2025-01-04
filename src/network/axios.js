9d04649c61988db6df06ed3d1db8c5f63e5e6c72
Elksass315 <qsasomar@gmail.com>
2025-01-04 11:24:55 +0200

    baseURL Fix

--- src/network/axios.js
+++ src/network/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/",
   // baseURL: "https://travel-api-5lpf.onrender.com/",
-  // baseURL: "http://10.15.156.8:3000/",
+  baseURL: "http://10.15.156.8:3000/",
   // baseURL: "https://ghost-honest-muskrat.ngrok-free.app/",
 });
